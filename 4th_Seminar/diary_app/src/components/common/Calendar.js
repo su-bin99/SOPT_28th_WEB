b4f5280b01d8b0e3c058097ce8fdcd03a4ad5c4f
Subin <sbsoobin@naver.com>
2021-06-06 14:49:27 +0900

    fix: 4차심화과제 수정

--- 4th_Seminar/diary_app/src/components/common/Calendar.js
+++ 4th_Seminar/diary_app/src/components/common/Calendar.js
@@ -37,7 +37,7 @@ const CalendarWrap = Styled.div`
       height: 57px;
       display: flex;
       /* flex-direction: row; */
-      /* justify-content: space-between; */
+      justify-content: space-between;
       align-items: center;
       width: 100%;
       overflow-x:scroll;
